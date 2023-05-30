c55a705b20c767ee5bba3bfcb6af0664ff925c36
hk221 <108066429+hk221@users.noreply.github.com>
2023-05-30 23:40:12 +0100

    jk

--- routdecision.py
+++ routdecision.py
@@ -26,12 +26,28 @@ def turn():
 def spinAround():
     # Spins the robot around 180 or 360 degrees?
     pass
+# Function to read LDR values from analog pins
 def getSensorReadings():
     # Returns a list of 4 numbers which would be 4 LDR readings, 
     # representing the distance to the nearest object in 
     # each direction (front, left, back, right)
     # The list should be in the same order as the directions
+    left_ldr_value = analogRead(left_ldr_pin)
+    right_ldr_value = analogRead(right_ldr_pin)
+    return [left_ldr_value, right_ldr_value]
     pass
+#from sensor readings store ldr values in variables like below
+#left_ldr_pin = 0  # Pin number for the left LDR input
+#right_ldr_pin = 1  # Pin number for the right LDR input
+# Function to check if there is an obstacle based on LDR readings
+def obstacle_detected():
+    ldr_values = getSensorReadings()
+    obstacle_threshold = 500  # Threshold value to determine obstacle presence
+    
+    if ldr_values[0] > obstacle_threshold or ldr_values[1] > obstacle_threshold:
+        return True
+    else:
+        return False
 def getMap():
     # Returns the robot's internal map
     pass
@@ -41,5 +57,14 @@ def getRobotPosition():
 def getRobotDirection():
     # Returns the robot's current direction
     pass
-
-
+def moveForward():
+    # Moves the robot forward by one square
+    pass
+# Main loop
+#while True:
+#    if obstacle_detected():
+#        # If obstacle detected, turn or take appropriate action
+#        turn()  
+#    else:
+#        # turn to face the direction of the next waypoint
+#        moveForward() 
