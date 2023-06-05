4607741a13e2cfe3ea817b98841c2882f5302ce7
hk221 <108066429+hk221@users.noreply.github.com>
2023-06-05 21:42:20 +0100

    decision

--- decision.v
+++ decision.v
@@ -1,35 +1,158 @@
-module decision(
-
-    // global clock & reset
-    input	clk;
-    input	reset_n;
-
-    // mm slave
-    input							s_chipselect;
-    input							s_read;
-    input							s_write;
-    output	reg	[31:0]	            s_readdata;
-    input	[31:0]				    s_writedata;
-    input	[2:0]					s_address;
-
-
-    // streaming sink
-    input	[23:0]                	sink_data;
-    input						    sink_valid;
-    output							sink_ready;
-    input							sink_sop;
-    input						    sink_eop;
-
-    // streaming source
-    output	[23:0]			  	   source_data;
-    output						   source_valid;
-    input						   source_ready;
-    output						   source_sop;
-    output						   source_eop;
-
-    // conduit export
-    input                         mode;
+module GraphSLAM (
+      input xADC_CLK_10,
+      input ARDUINO_IO_15,
+      input ARDUINO_IO_14,
+      input ARDUINO_IO_13,
+      input ARDUINO_IO_12,
+      input ARDUINO_IO_11,
+      input ARDUINO_IO_10,
+      input ARDUINO_IO_9,
+      input ARDUINO_IO_8,
+      input ARDUINO_IO_7,
+      input ARDUINO_IO_6,
+      input ARDUINO_IO_5,
+      input ARDUINO_IO_4,
+      input ARDUINO_IO_3,
+      input ARDUINO_IO_2,
+      input ARDUINO_IO_1,
+      input ARDUINO_IO_0,
+      input ARDUINO_RESET_N,
+      input CAMERA_I2C_SCL,
+      input CAMERA_I2C_SDA,
+      output CAMERA_PWDN_n,
+      output [12:0] DRAM_ADDR,
+      output [1:0] DRAM_BA,
+      output DRAM_CAS_N,
+      output DRAM_CKE,
+      output DRAM_CLK,
+      output DRAM_CS_N,
+      inout [15:0] DRAM_DQ,
+      output DRAM_LDQM,
+      output DRAM_RAS_N,
+      output DRAM_UDQM,
+      output DRAM_WE_N,
+      output GSENSOR_CS_N,
+      input [2:0] GSENSOR_INT,
+      output GSENSOR_SCLK,
+      inout GSENSOR_SDI,
+      inout GSENSOR_SDO,
+      output [7:0] HEX0,
+      output [7:0] HEX1,
+// Other module ports...
 );
 
+  // Assign the pin names to signals or variables
+  assign xADC_CLK_10 = PIN_N5;
+  assign ARDUINO_IO_15 = PIN_AA20;
+  assign ARDUINO_IO_14 = PIN_AB21;
+  assign ARDUINO_IO_13 = PIN_AB20;
+  assign ARDUINO_IO_12 = PIN_Y19;
+  assign ARDUINO_IO_11 = PIN_AA19;
+  assign ARDUINO_IO_10 = PIN_AB19;
+  assign ARDUINO_IO_9 = PIN_AA17;
+  assign ARDUINO_IO_8 = PIN_AB17;
+  assign ARDUINO_IO_7 = PIN_AA12;
+  assign ARDUINO_IO_6 = PIN_AA11;
+  assign ARDUINO_IO_5 = PIN_Y10;
+  assign ARDUINO_IO_4 = PIN_AB9;
+  assign ARDUINO_IO_3 = PIN_AB8;
+  assign ARDUINO_IO_2 = PIN_AB7;
+  assign ARDUINO_IO_1 = PIN_AB6;
+  assign ARDUINO_IO_0 = PIN_AB5;
+  assign ARDUINO_RESET_N = PIN_F16;
+  assign CAMERA_I2C_SCL = PIN_AA7;
+  assign CAMERA_I2C_SDA = PIN_Y6;
+  assign CAMERA_PWDN_n = PIN_Y7;
+  assign DRAM_ADDR[12] = PIN_R20;
+  assign DRAM_ADDR[11] = PIN_P20;
+  assign DRAM_ADDR[10] = PIN_T20;
+  assign DRAM_ADDR[9] = PIN_P19;
+  assign DRAM_ADDR[8] = PIN_P18;
+  assign DRAM_ADDR[7] = PIN_R18;
+  assign DRAM_ADDR[6] = PIN_T19;
+  assign DRAM_ADDR[5] = PIN_T18;
+  assign DRAM_ADDR[4] = PIN_R19;
+  assign DRAM_ADDR[3] = PIN_N18;
+  assign DRAM_ADDR[2] = PIN_N19;
+  assign DRAM_ADDR[1] = PIN_M18;
+  assign DRAM_ADDR[0] = PIN_L19;
+  assign DRAM_BA[1] = PIN_P17;
+  assign DRAM_BA[0] = PIN_P16;
+  // Other pin assignments...
+
+  // Your module logic goes here
+
+  // Add internal registers and memories for storing graph data
+  reg [7:0] vertex_data[0:255];  // Example: 8-bit vertex data array
+  reg [7:0] edge_data[0:255];    // Example: 8-bit edge data array
+
+  // Define state machine for controlling the GraphSLAM algorithm
+  reg [2:0] state;
+  localparam STATE_IDLE = 3'b000;
+  localparam STATE_READ_VERTEX = 3'b001;
+  localparam STATE_READ_EDGE = 3'b010;
+  // ...
+
+  // Add computational blocks for graph optimization, data association, etc.
+  // Example: Graph optimization using Sparse Bundle Adjustment (SBA)
+  // ...
+
+  // Add logic for reading/writing data to/from external memory
+  // Example: Read vertex data from external memory
+  reg [7:0] memory_addr;
+  reg [7:0] memory_data;
+  // ...
+
+  // Add logic for synchronization and timing constraints
+  // ...
+
+  // Implement the functionality of the GraphSLAM algorithm
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      // Reset internal registers and memories
+      vertex_data <= (8'h0);
+      edge_data <= (8'h0);                      
+      state <= STATE_IDLE;
+      // ...
+    end else begin
+      // Perform GraphSLAM computations based on the state machine
+      case (state)
+        STATE_IDLE: begin
+          // Wait for external trigger to start GraphSLAM
+          if (start_trigger) begin
+            state <= STATE_READ_VERTEX;
+            memory_addr <= 8'h00;
+          end
+        end
+
+        STATE_READ_VERTEX: begin
+          // Read vertex data from external memory
+          memory_data <= read_memory(memory_addr);
+          vertex_data[memory_addr] <= memory_data;
+          memory_addr <= memory_addr + 1;
+
+          if (memory_addr == 8'hFF) begin
+            state <= STATE_READ_EDGE;
+            memory_addr <= 8'h00;
+          end
+        end
+
+        STATE_READ_EDGE: begin
+          // Read edge data from external memory
+          memory_data <= read_memory(memory_addr);
+          edge_data[memory_addr] <= memory_data;
+          memory_addr <= memory_addr + 1;
+
+          if (memory_addr == 8'hFF) begin
+            state <= STATE_IDLE;
+            // Perform graph optimization and other computations
+            // ...
+          end
+        end
+
+        // ...
+      endcase
+    end
+  end
 
 endmodule
