f4acf173da3ee4c0a34ff2452a7f5e8cc087a387
hk221 <108066429+hk221@users.noreply.github.com>
2023-06-15 22:21:06 +0100

    updated

--- arduino.cpp
+++ arduino.cpp
@@ -2,6 +2,7 @@
 #include <Arduino_JSON.h>
 #include <ArduinoHttpClient.h>
 #include <HardwareSerial.h>
+#include <string.h>
 
 #define RX_PIN 16
 #define TX_PIN 17
@@ -10,17 +11,15 @@ HardwareSerial SerialPort(2);
 
 char ssid[] = "96 Dalling Road";
 char pass[] = "Panda123";
-char server[] = "54.227.172.163";
-byte ip[] = {54, 227, 172, 163};
+char server[] = "18.212.197.92";
+byte ip[] = { 18, 212, 197, 92 };
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, server, 3000);
 
-float x = 250;
+String x;
 
 void setup() {
-  Serial.begin(9600);
-  Serial2.begin(9600);
-  SerialPort.begin(9600,SERIAL_8N1, 16,17);
+  Serial.begin(115200, SERIAL_8N1, 16, 17);
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
@@ -31,40 +30,38 @@ void setup() {
   Serial.println("Connected to WiFi");
   printWifiStatus();
   Serial.println("\nStarting connection to server...");
-}
-
-void loop() {
-  x=receiveDataFromFPGA();
-  Serial.println("Received coordinates: ");
-  Serial.println(x);
-  sendData(x);
-}
-
-float receiveDataFromFPGA() {
-  while (Serial2.available()) {
-    // Wait until at least 1 byte is available to read
-    //Serial.println("hrello");
-  }
-
-  return float(Serial2.read());
-}
-
-void sendData(float y) {
   while (!client.connected()) {
-    Serial.println("Connecting to server...");
+  Serial.println("Connecting to server...");
     if (client.connect(ip, 3000)) {
       Serial.println("Connected to server");
-    } else {
+    } 
+    else {
       Serial.println("Connection failed");
     }
   }
+}
 
-  JSONVar imageJson;
-  imageJson["accX"] = x;
-  String accString = JSON.stringify(imageJson);
+void loop() {
+  receiveDataFromFPGA();
+  sendData(x);
+
+}
+
+void receiveDataFromFPGA() {
+  if (Serial.available()) {
+    x = Serial.readStringUntil('\n');
+    Serial.println("Received coordinates: ");
+    Serial.println(x);
+  }
+}
+
+void sendData(String y) {
+  // JSONVar imageJson;
+  // imageJson["accX"] = x;
+  // String accString = JSON.stringify(imageJson);
 
   // Send the image data to the server
-  client.post("/acc", "application/json", accString);
+  client.post("/acc", "application/json", y);
   String response = client.responseBody();
   Serial.println(response);
 }
