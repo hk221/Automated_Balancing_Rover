03940032e422c2058fc83b484e29fbb299b6f9fa
aaashah <aisha@xshah.com>
2023-06-21 23:53:55 +0100

    updated again

--- search.py
+++ search.py
@@ -1,12 +1,20 @@
-import serial
-import numpy as np
+import socket
 import time
 from heapq import heappop, heappush
+import asyncio
+import websockets
 
-# Serial communication setup
-arduino_port = 'COM3'  # Change to the appropriate port
-arduino_baudrate = 115200
-ser = serial.Serial(arduino_port, arduino_baudrate, timeout=1)
+async def read_from_websocket():
+    async with websockets.connect('ws://localhost:8765') as websocket: # Replace with your WebSocket server URL
+        while True:
+            response = await websocket.recv()
+            # Process the received message
+            #print(decision)  # Example: Print the received message
+# Socket communication setup
+server_address = 'localhost'  # Change to the appropriate server address
+server_port = 1234  # Change to the appropriate server port
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((server_address, server_port))
 
 # Maze dimensions
 maze_width = 280
@@ -88,9 +96,7 @@ def update_path(current_pos, next_pos, path):
 
 # Main program
 def main():
-    # Initialize the walls array
-   # walls = np.zeros((maze_height, maze_width), dtype=bool)
-
+   
     # Run Dijkstra's algorithm
     distances, previous = dijkstra(walls, start_position, goal_position)
 
@@ -101,53 +107,128 @@ def main():
     while current_pos != start_position:
         path.append(current_pos)
         current_pos = previous[current_pos]
-    #path.append(start_position)
-    #path.reverse()
+    path.append(start_position)
+    path.reverse()
 
     # Execute the movements based on the shortest path
     for i in range(len(path)-1):
         current_pos = path[i]
         next_pos = path[i+1]
         
+        #define decision
+        response=read_from_websocket()
         # Get the current and next coordinates
         current_coordinates = get_current_coordinates()
         next_coordinates = get_next_coordinates(next_pos)
-
-        # Determine the movement direction based on sensor readings from Arduino
-        ser.write(b'get_led_walls\n')  # Request sensor readings from Arduino
-        response = ser.readline().decode().strip()  # Read response from Arduino
-        # Use the sensor readings to update the walls array dynamically
-
+        
         # Process the sensor readings to update the walls array
+        # this part is just to update the walls- not to do with decision!!
         bits = int(response, 2)
         row, col = current_pos
         if bits & 0b0001:
-            walls[row-1, col] = True  # Wall detected in front
+            # Wall detected in the front
+            walls[row-1, col] = True
         if bits & 0b0010:
-            walls[row+1, col] = True  # Wall detected at the back
+            # Wall detected at the back
+            walls[row+1, col] = True
         if bits & 0b0100:
-            walls[row, col+1] = True  # Wall detected on the right
+            # Wall detected on the right
+            walls[row, col+1] = True
         if bits & 0b1000:
-            walls[row, col-1] = True  # Wall detected on the left
+            # Wall detected on the left
+            walls[row, col-1] = True
+        if bits & 0b0101:
+            # Walls detected at the front and right
+            walls[row-1, col+1] = True
+        if bits & 0b1010:
+            # Walls detected at the back and left
+            walls[row+1, col-1] = True
+        if bits & 0b0110:
+            # Walls detected at the right and left
+            walls[row, col+1] = True
+            walls[row, col-1] = True
+        if bits & 0b1001:
+            # Walls detected at the front and left
+            walls[row-1, col-1] = True
+        if bits & 0b0111:
+            # Walls detected at the front, right, and left
+            walls[row-1, col+1] = True
+            walls[row, col-1] = True
+        if bits & 0b1011:
+            # Walls detected at the back, right, and left
+            walls[row+1, col+1] = True
+            walls[row, col-1] = True
+        if bits & 0b1101:
+            # Walls detected at the front, back, and left
+            walls[row-1, col-1] = True
+            walls[row+1, col-1] = True
+        if bits & 0b1110:
+            # Walls detected at the front, back, and right
+            walls[row-1, col+1] = True
+            walls[row+1, col+1] = True
+        if bits & 0b1111:
+            # Walls detected in all four directions
+            walls[row-1, col-1] = True
+            walls[row+1, col+1] = True
+            walls[row, col+1] = True
+            walls[row, col-1] = True
 
-        # Execute the movement commands based on the determined direction
-        if current_pos[0] < next_pos[0]:
-            move_forward()
-        elif current_pos[0] > next_pos[0]:
-            move_back()
-        elif current_pos[1] < next_pos[1]:
-            turn_right()
-        elif current_pos[1] > next_pos[1]:
-            turn_left()
         
+        # Determine the movement commands based on the determined direction and sensor readings
+        if current_pos[0] < next_pos[0]:
+        # Move forward
+            if walls[current_pos[0]-1, current_pos[1]] and walls[current_pos[0], current_pos[1]-1]:
+                # Wall on the left and front, turn right
+                decision= "turn_right()"
+            elif walls[current_pos[0]-1, current_pos[1]]:
+                # Wall in front, turn left
+                decision= "turn_left()"
+            elif walls[current_pos[0], current_pos[1]-1]:
+                # Wall on the left, turn right
+                decision= "turn_right()"
+            else:
+                decision= "move_forward()"
 
-        update_path(current_pos, next_pos)
+        elif current_pos[1] < next_pos[1]:
+        # Turn right
+            if walls[current_pos[0]-1, current_pos[1]] and walls[current_pos[0]+1, current_pos[1]]:
+                # Wall in front and back, turn back
+                decision= "move_back()"
+            elif walls[current_pos[0]-1, current_pos[1]]:
+                # Wall in front, turn left
+                decision= "turn_left()"
+            elif walls[current_pos[0]+1, current_pos[1]]:
+                # Wall at the back, turn right
+                decision= "turn_right()"
+            else:
+                decision= "turn_right()"
 
-        # Add a delay to allow the robot to complete the movement
-        time.sleep(1)
+        elif current_pos[1] > next_pos[1]:
+        # Turn left
+            if walls[current_pos[0]-1, current_pos[1]] and walls[current_pos[0]+1, current_pos[1]]:
+                # Wall in front and back, turn back
+                decision= "move_back()"
+            elif walls[current_pos[0]-1, current_pos[1]]:
+                # Wall in front, turn right
+                decision= "turn_right()"
+            elif walls[current_pos[0]+1, current_pos[1]]:
+                # Wall at the back, turn left
+                decision= "turn_left()"
+            else:
+                decision= "turn_left()"
 
-    # Close the serial connection
-    ser.close()
+            # Determine the movement direction based on sensor readings from Arduino
+            #sock.sendall(b'get_led_walls\n')            
+            # Use the sensor readings to update the walls array dynamically
+            
+        sock.sendall(decision.encode())
+        
+        time.sleep(1)  # Add a delay to allow the robot to complete the movement
+
+        update_path(current_pos, next_pos, path)
+
+    # Close the socket connection
+    sock.close()
 
 if __name__ == '__main__':
     main()
